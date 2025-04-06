942fe2fe470a0aa48d85a32e349e48e75a8f71c7
Chris <banksjazz87@gmail.com>
2025-04-05 21:39:15 -0400

    feature: I have results from the postgress database being displayed on the client

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,22 @@
+"use client"
 
+import { useEffect, useState } from "react";
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!);
 
 export default function Login() {
+    const [notes, setNotes] = useState([]);
+
+    useEffect(() => {
+        getNotes();
+    }, []);
+
+    async function getNotes() {
+        const data = await supabase.from("notes").select();
+        
+        console.log("HERE ", data);
+    }
     return (
         <h1>This will be the login page</h1>
     )
