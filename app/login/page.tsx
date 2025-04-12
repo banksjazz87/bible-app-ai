db008287b0f35966e03d3e64e46e6dccf39aeccb
Chris <banksjazz87@gmail.com>
2025-04-12 08:25:30 -0400

    feature: working on setting up the auth for the bible app

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,23 +1,45 @@
-"use client"
+"use client";
 
 import { useEffect, useState } from "react";
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from "@/utils/supabase/client";
+import { login, signup } from "./actions";
 
-const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!);
+const supabase = createClient();
 
 export default function Login() {
-    const [notes, setNotes] = useState([]);
+	const [notes, setNotes] = useState([]);
 
-    useEffect(() => {
-        getNotes();
-    }, []);
+	useEffect(() => {
+		getNotes();
+	}, []);
 
-    async function getNotes() {
-        const data = await supabase.from("notes").select();
-        
-        console.log("HERE ", data);
-    }
-    return (
-        <h1>This will be the login page</h1>
-    )
+	async function getNotes() {
+		const data = await supabase.from("notes").select();
+
+		console.log("HERE ", data);
+	}
+	return (
+		<main>
+			<h1>This will be the login page</h1>
+
+			<form>
+				<label htmlFor="email">Email:</label>
+				<input
+					id="email"
+					name="email"
+					type="email"
+					required
+				/>
+				<label htmlFor="password">Password:</label>
+				<input
+					id="password"
+					name="password"
+					type="password"
+					required
+				/>
+				<button formAction={login}>Log in</button>
+				<button formAction={signup}>Sign up</button>
+			</form>
+		</main>
+	);
 }
