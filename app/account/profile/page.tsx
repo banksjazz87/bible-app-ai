ca18046a0fd5b78390614efbdd6063370978d8d3
Chris <banksjazz87@gmail.com>
2025-07-14 08:20:34 -0400

    refactor: looking into refactoring a component to be a server component

--- app/account/profile/page.tsx
+++ app/account/profile/page.tsx
@@ -11,17 +11,18 @@ export default function ProfilePage() {
     async function getThreads():  Promise<APIDataResponse<ChatThread[] | null>>{ 
         const data = await fetch('/account/api/profile/pastThreads');
         const jsonData: APIDataResponse<ChatThread[] | null> = await data.json();
-        console.log('HERE ', jsonData);
         return jsonData;
     }
-    const pastThreadData = getThreads();
 
+    const pastThreadData =  getThreads();
+
+    
 	return (
 		<main>
 			<p>{`The user is currently logged in ${loggedInDetails.isLoggedIn} and his username is ${loggedInDetails.userName}`}</p>
             <section className="mt-16 flex flex-col gap-4">
                 <h2 className="font-bold text-2xl">Chat History</h2>
-				<Suspense fallback={<PastThreadsSkeleton />}>
+                <Suspense fallback={<PastThreadsSkeleton />}>
 					<PastThreads threads={pastThreadData} />
 				</Suspense>
 			</section>
