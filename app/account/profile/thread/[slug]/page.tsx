072b030b36bb1be7971db2186462841e0b3f2240
Chris <banksjazz87@gmail.com>
2025-07-16 21:34:44 -0400

    style: created the styling for the thread page

--- app/account/profile/thread/[slug]/page.tsx
+++ app/account/profile/thread/[slug]/page.tsx
@@ -6,27 +6,16 @@ import { convertDateTime } from "@/lib/utils";
 import { LLMReqObject } from "../../../../../lib/definitions";
 import { retrieveBibleChapter } from "@/lib/bible/bibleMethods";
 import BibleVerses from "@/app/bible/components/BibleVerses";
+import { Button } from "@/components/ui/button";
 
 export default async function Page(props: { params: Promise<{ slug: string }> }) {
 	const params = await props.params;
 	const slug = params.slug;
 	const thread = await GetSingleThread(slug);
 	const threadJSON: APIDataResponse<ChatThread> = await thread.json();
-	const {
-		thread_name,
-		book,
-		chapter, 
-		date_created,
-		start_verse,
-		end_verse,
-		thread_slug,
-		user_notes,
-		llm_notes, 
-		bible_version
-	} = threadJSON.data;
+	const { thread_name, book, chapter, date_created, start_verse, end_verse, thread_slug, user_notes, llm_notes, bible_version } = threadJSON.data;
 
 	const bibleText = await retrieveBibleChapter(bible_version, book, chapter);
-
 
 	const date = convertDateTime(date_created);
 
@@ -34,8 +23,11 @@ export default async function Page(props: { params: Promise<{ slug: string }> })
 		return llmNotes.map((x: LLMReqObject, y: number): JSX.Element | undefined => {
 			if (x.output.length > 0) {
 				return (
-					<div key={`llm_heading_${y}`}>
-						<h2>{x.heading}</h2>
+					<div
+						key={`llm_heading_${y}`}
+						className="flex flex-col gap-2"
+					>
+						<h2 className="text-2xl font-extrabold">{x.heading}</h2>
 						<div
 							className="llm_content"
 							dangerouslySetInnerHTML={{ __html: x.output }}
@@ -49,31 +41,52 @@ export default async function Page(props: { params: Promise<{ slug: string }> })
 	if (!thread) {
 		return (
 			<Suspense fallback={<div>Loading...</div>}>
-				<h1>No threads found</h1>
+				<h1 className="font-extrabold text-3xl">No threads found</h1>
 			</Suspense>
 		);
 	}
 
 	return (
 		<Suspense fallback={<div>Loading...</div>}>
-			<main>
-				<section>
-					<h1>{thread_name}</h1>
-					<p>{date}</p>
-					<div>
-						<p>{`${book} ${chapter}:${start_verse} - ${end_verse}`}</p>
-						<Suspense fallback={<div><p>Loading bible data..</p></div>}>
-							{bibleText && 
-								<BibleVerses
-								versesArray={bibleText.data}
-								startVerse={start_verse}
-								endVerse={end_verse}
-							/>
+			<main className="mt-16">
+				<section className="mt-16 flex flex-col gap-8">
+					<div className="flex flex-col gap-2">
+						<div className="flex flex-row justify-between">
+							<h1 className="capitalize font-extrabold text-3xl">{thread_name}</h1>
+
+							<div className="flex flex-row gap-2">
+								<Button>Download</Button>
+								<Button>Print</Button>
+								<Button>Edit</Button>
+							</div>
+						</div>
+						<p className="font-extrabold">{date}</p>
+					</div>
+					<div className="flex flex-col gap-2">
+						<h2 className="capitalize font-extrabold text-2xl">{`${book} ${chapter}:${start_verse} - ${end_verse}`}</h2>
+						<Suspense
+							fallback={
+								<div>
+									<p>Loading bible data..</p>
+								</div>
 							}
+						>
+							{bibleText && (
+								<BibleVerses
+									versesArray={bibleText.data}
+									startVerse={start_verse}
+									endVerse={end_verse}
+								/>
+							)}
 						</Suspense>
 					</div>
 					<div>
-						<p>{`User Notes here ${user_notes}`}</p>
+						{user_notes.length > 0 && (
+							<div className="flex flex-col gap-2">
+								<h2 className="text-2xl font-extrabold">User Notes</h2>
+								<p>{user_notes}</p>
+							</div>
+						)}
 					</div>
 					<div>{LLMNotes(llm_notes)}</div>
 				</section>
