bc9ad3b5c2da8694682f23ae7d2b45be177655f7
Chris <banksjazz87@gmail.com>
2025-09-20 16:10:52 -0400

    feature: I created a price page, and found an issue with the max requests within the bible page

--- app/account/subscribe/form/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function SubscribeForm() {
-    return (
-        <h1>This is the subscribe form</h1>
-    )
-}
---
+++ app/subscribe/page.tsx
@@ -0,0 +1,9 @@
+
+
+
+
+export default function SubscriptionPage() {
+    return (
+        <p>This will have a form for the user to fill out to subscribe.</p>
+    )
+}
