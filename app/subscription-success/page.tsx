30e0f9eca3daebdc1ffaded5a89561d65ca3403f
Chris <banksjazz87@gmail.com>
2025-10-25 09:09:28 -0400

    feature: working on the stripe integration

--- app/subscription-success/page.tsx
+++ app/subscription-success/page.tsx
@@ -1,31 +1,46 @@
 "use client";
 
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
+function PageContent() {
+	const searchParams = useSearchParams();
+	const sessionID = searchParams.get("session-id");
 
+	return (
+		<section>
+			<div className="product Box-root">
+				<div className="description Box-root">
+					<h3>Subscription to Test Plan Successful!</h3>
+				</div>
+			</div>
+			<form
+				action="/create-portal-session"
+				method="POST"
+			>
+				<input
+					type="hidden"
+					id="session-id"
+					name="session_id"
+					value={sessionID ? sessionID : ""}
+				/>
+				<button
+					id="checkout-and-portal-button"
+					type="submit"
+				>
+					Manage your billing information.
+				</button>
+			</form>
+		</section>
+	);
+}
 
 export default function SubscriptionSuccessPage() {
-    const serachParams = useSearchParams();
-    const sessionID = serachParams.get('session-id');
-
-    return (
-        <section>
-            <div className="product Box-root">
-                <div className="description Box-root">
-                    <h3>Subscription to Test Plan Successful!</h3>
-                </div>
-            </div>
-            <form action="/create-portal-session" method="POST">
-                <input
-                    type="hidden"
-                    id="session-id"
-                    name="session_id"
-                    value={sessionID ? sessionID : ''}
-                />
-                <button id="checkout-and-portal-button" type="submit">
-                    Manage your billing information.
-                </button>
-            </form>
-        </section>
-    )
+	return (
+		<Suspense fallback="<p>Loading...</p>">
+			<main>
+				<PageContent />
+			</main>
+		</Suspense>
+	);
 }
