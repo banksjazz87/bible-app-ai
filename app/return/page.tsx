4fe762dcf0662a7644dfba64ba4bf2dfa3d0c373
Chris <banksjazz87@gmail.com>
2025-12-21 15:49:49 -0500

    feature: I have the invoice object being returned correctly on the return page

--- app/return/page.tsx
+++ app/return/page.tsx
@@ -2,14 +2,32 @@
 
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { stripe } from "@/lib/stripe";
+import Stripe from "stripe";
 
-export default async function Page() {
+
+export default async function Page({ searchParams }: { searchParams: Promise<{ session_id: string }> }) {
+    const sessionID: string = (await searchParams).session_id;
+    const sessionData: Stripe.Response<Stripe.Checkout.Session> = await stripe.checkout.sessions.retrieve(sessionID);
+    const { customer, customer_details, amount_total, payment_status, invoice } = sessionData;
+    const customerEmail = customer_details?.email;
+    const customerName = customer_details?.name;
+    const orderMessage = payment_status === "paid" ? "Thanks for Subscribing!" : "Your subscription is pending.";
+
+    // const invoice = await stripe.invoices.createPreview({ customer: customer as string });
+    const invoiceData = invoice ? await stripe.invoices.retrieve(invoice as string) : null;
+    console.log('Invoice Data here ', invoiceData);
+
+
+    console.log('Data here ', sessionData);
+    
     return (
 			<main className="flex flex-col">
 				<section className="py-16">
-					<h1 className="font-mono font-extrabold text-5xl text-center">Thanks for your order</h1>
+                <h1 className="font-mono font-extrabold text-5xl text-center">{orderMessage}</h1>
 				</section>
-				<section className="py-16">
+            <section className="py-16">
+                <p className="text-center">{`Thanks for your subscription to the Bible App ${customerName}!  We're so happy to have you with us.  If you would ever like to change or cancel your subscription you can do so within your account page.  Thanks again!  We're excited to have you on board.` }</p>
 					<div className="flex flex-row gap-4 justify-center pt-9">
 						<Button>
 							<Link href="/bible">Go to App</Link>
