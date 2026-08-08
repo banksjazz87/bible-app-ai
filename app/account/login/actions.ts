022cd88cc46dddd0349148adbdc812599966e034
Chris <banksjazz87@gmail.com>
2026-08-08 06:48:58 -0400

    feature: creating functionality in the log in form that if the user is authenticated, and not present in the user table, we will add them

--- app/account/login/actions.ts
+++ app/account/login/actions.ts
@@ -3,27 +3,55 @@
 import { revalidatePath } from "next/cache";
 import { createClient } from "@/utils/supabase/server";
 import { APIResponse, APIDataResponse, LoginForm, UserData } from "@/lib/definitions";
-
+import User from "@/lib/classes/User";
 
 export async function login(formData: LoginForm): Promise<APIResponse | APIDataResponse<UserData>> {
-    const supabase = await createClient();
-    
-    const { error, data } = await supabase.auth.signInWithPassword(formData);
-    if (error) {
+	const supabase = await createClient();
+
+	const { error, data } = await supabase.auth.signInWithPassword(formData);
+	if (error) {
 		return {
 			status: 404,
-			message: error.message
+			message: error.message,
 		};
-    }
+	}
 
-	revalidatePath('/');
+	//Verify the user has user roles associated with them, if not, we will add them to the free tier.
+	const user = new User();
+	const userRoles = await user.getUserDetails();
+
+	if (!userRoles) {
+		return {
+			status: 400,
+			message: 'The user roles were unable to be retrieved'
+		}
+	}
+
+	if (userRoles && userRoles.status !== 200) {
+		const { error } = await supabase.from("user_roles").insert({
+			user_id: data.user.id,
+			super_admin: false,
+			standard_tier: false,
+			free_tier: true,
+			email_address: data.user.email,
+			premiere_tier: false,
+		});
+
+		if (error) {
+			return {
+				status: 400,
+				message: `Unable to add the user to the user table due to the following: ${error}`,
+			};
+		}
+	}
+
+	revalidatePath("/");
 	return {
 		status: 200,
 		message: `The user is verified, ${data.user.id}`,
 		data: {
 			email: data.user.email as string,
 			id: data.user.id,
-		}
-	}
+		},
+	};
 }
-
--- app/api/user-roles/route.ts
+++ app/api/user-roles/route.ts
@@ -3,36 +3,35 @@ import { createClient } from "../../../utils/supabase/server";
 import { APIDataResponse } from "@/lib/definitions";
 import { UserRoles } from "@/lib/definitions";
 
-
 export async function GET() {
-    const supabase = await createClient();
-    const {
-        data: { user },
-        error,
-    } = await supabase.auth.getUser();
+	const supabase = await createClient();
+	const {
+		data: { user },
+		error,
+	} = await supabase.auth.getUser();
 
-    const responseData: APIDataResponse<unknown[] | null> = {
-        status: 500,
-        message: "This user is not found",
-        data: null,
-    };
+	const responseData: APIDataResponse<unknown[] | null> = {
+		status: 500,
+		message: "This user is not found",
+		data: null,
+	};
 
-    if (!error && user) {
-        const userId = user.id;
-        const { data, error } = await supabase.from("user_roles").select("*").eq("user_id", userId);
+	if (!error && user) {
+		const userId = user.id;
+		const { data, error } = await supabase.from("user_roles").select("*").eq("user_id", userId);
 
-        if (!error) {
-            responseData.status = 200;
-            responseData.message = "User roles fetched successfully";
-            responseData.data = data ? (data as UserRoles[]) : null;
-        } else {
-            responseData.status = 400;
-            responseData.message = `The following error occurred, ${error.message}`;
-        }
-    } else if (error) {
-        responseData.status = 400;
-        responseData.message = `Error fetching user: ${error.message}`;
-    }
+		if (!error) {
+			responseData.status = 200;
+			responseData.message = "User roles fetched successfully";
+			responseData.data = data ? (data as UserRoles[]) : null;
+		} else {
+			responseData.status = 400;
+			responseData.message = `The following error occurred, in retrieving the user: ${error}`;
+		}
+	} else if (error) {
+		responseData.status = 400;
+		responseData.message = `Error fetching user: ${error.message}`;
+	}
 
-    return NextResponse.json(responseData);
+	return NextResponse.json(responseData);
 }
