e4fe04dfea7bf738078794162bd6aa8d599f1eaa
Chris <banksjazz87@gmail.com>
2026-09-01 06:46:24 -0400

    feature: working on the cancel subscription method

--- app/account/subscription/components/SubscriptionLayout.tsx
+++ app/account/subscription/components/SubscriptionLayout.tsx
@@ -4,11 +4,13 @@ import { use, useState } from "react";
 import { UserSubscriptionResponse } from "@/lib/definitions";
 import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { Stripe } from "stripe";
+import { cancelSubscription } from "@/app/actions/stripe";
 import { Spinner } from "@/components/ui/spinner";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Button } from "@/components/ui/button";
 import { StripeProducts } from "@/lib/constants";
+import Alert from "@/app/ui/Alert";
 
 
 type SubscriptionLayoutProps = {
@@ -19,17 +21,33 @@ export default function SubscriptionLayout({ subscriptionData }: SubscriptionLay
     const userData = use(subscriptionData);
     const [isCancelling, setIsCancelling] = useState<boolean>(false);
     const [cancelID, setCancelID] = useState<string>('');
+    const [alertIsOpen, setAlertIsOpen] = useState<boolean>(false);
+
     console.log('User subscription data here!!!!! ', userData);
 
-    const cancelHandler = (id: string) => {
+    function cancelRequestHandler(id: string): void {
         setIsCancelling(true);
         setCancelID(id);
-
-        setTimeout(() => {
-            setIsCancelling(false);
-            setCancelID('');
-        }, 2000);
+        setAlertIsOpen(true);
     };
+
+    function clearCancelSubscriptionState(): void {
+        setIsCancelling(false);
+        setCancelID('');
+        setAlertIsOpen(false);
+    }
+
+    async function cancelSubscriptionHandler() {
+        const cancel = await cancelSubscription(cancelID);
+
+        if (cancel.status !== 200) {
+            clearCancelSubscriptionState();
+            console.error('The following error occurred in canceling the subscription', cancel.message);
+        } else {
+            clearCancelSubscriptionState();
+            console.log(cancel.message);
+        }
+    }
 
     const getDate = (unixDate: number): string => {
         const date = new Date(unixDate * 1000);
@@ -42,6 +60,18 @@ export default function SubscriptionLayout({ subscriptionData }: SubscriptionLay
 			<h2>Subscription Details</h2>
 
 			{userData.data === null && <p>No Data found</p>}
+
+			<Alert
+				isOpen={alertIsOpen}
+				openHandler={() => setAlertIsOpen(true)}
+				closeHandler={() => setAlertIsOpen(false)}
+				title="Cancel Subscription"
+				description="Are you sure that you would like to cancel the subscription?"
+				cancelHandler={() => setAlertIsOpen(false)}
+				confirmHandler={() => cancelSubscriptionHandler()}
+				confirmText="Cancel Subscription"
+				cancelText="Keep Subscription"
+			/>
 
 			{userData.data !== null && (
 				<Table>
@@ -66,17 +96,18 @@ export default function SubscriptionLayout({ subscriptionData }: SubscriptionLay
 								<TableCell>{"-"}</TableCell>
 								<TableCell className="capitalize"></TableCell>
 								<TableCell>
-									{/* <Button
-										variant="outline"
-										onClick={(): void => cancelHandler()}
-										disabled={isCancelling && cancelID === thread.id}
+									<Button
+										variant="destructive"
+										onClick={(): void => cancelRequestHandler(data.id)}
+										disabled={isCancelling && cancelID === data.id}
 									>
-										{isCancelling && cancelID === thread.id && <Spinner data-icon="inline-start" />}
-										<FontAwesomeIcon
+										{isCancelling && cancelID === data.id && <Spinner data-icon="inline-start" />}
+										{/* <FontAwesomeIcon
 											icon={faTrash}
 											className="text-gray-700"
-										/>
-									</Button> */}
+										/> */}
+										Cancel
+									</Button>
 								</TableCell>
 							</TableRow>
 						))}
