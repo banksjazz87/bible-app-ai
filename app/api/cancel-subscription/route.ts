6628269384dca7901c4a86231f067466f23acca9
Chris <banksjazz87@gmail.com>
2026-09-08 06:47:17 -0400

    feature: continuing to work on adding the data to the cancellations table once a user decides to cancel their subscription

--- app/api/cancel-subscription/route.ts
+++ app/api/cancel-subscription/route.ts
@@ -2,45 +2,58 @@
 import { NextResponse } from "next/server";
 
 /**
- * 
- * @param request 
+ *
+ * @param request
  * @returns {Promise<NextResponse>}
  * @description route used to update the cancellation table to keep track of which users have requested to cancel their account.
  */
 
 export async function POST(request: Request): Promise<NextResponse> {
-    const body = await request.json();
-    const { userID, cancelRequestedOn, cancelOn } = body;
+	const body = await request.json();
+	const { user_id, cancel_requested_on, cancel_end_date } = body;
+	console.log("user id = ", user_id);
+	console.log("cancel requested on ", cancel_requested_on);
+	console.log("cancel on ", cancel_end_date);
+    console.log("body = ", body);
+    
+    const cancelRequestedOnDate = new Date(cancel_requested_on * 1000);
+    const requestedDate = cancelRequestedOnDate.toISOString();
 
-    const supabaseURL = process.env.NEXT_PUBLIC_SUPABASE_URL;
-    const anonKey = process.env.NEXT_SUPABASE_ANON_KEY;
-    const apiKey = process.env.NEXT_SUPABASE_API_KEY;
+    const cancelOnDate = new Date(cancel_end_date * 1000);
+    const endDate = cancelOnDate.toISOString();
 
-    const targetTable = "cancellations";
-    const supaUrl = `${supabaseURL}/rest/v1/${targetTable}`;
+    console.log('Cancel on Date: ', cancelOnDate.toISOString());
 
-    try {
-        const response = await fetch(supaUrl, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                apiKey: apiKey as string,
-                Authorization: anonKey as string,
-            },
-            body: JSON.stringify({
-                user_id: userID,
-                cancel_requested_on: cancelRequestedOn,
-                cancel_end_date: cancelOn,
-            }),
-        });
+	const supabaseURL = process.env.NEXT_PUBLIC_SUPABASE_URL;
+	const anonKey = process.env.NEXT_SUPABASE_ANON_KEY;
+	const apiKey = process.env.NEXT_SUPABASE_API_KEY;
 
-        if (!response.ok) {
-            throw new Error(`HTTP error!  Status: ${response.status}`);
-        }
+	const targetTable = "cancellations";
+	const supaUrl = `${supabaseURL}/rest/v1/${targetTable}`;
 
-        return NextResponse.json({ message: "The user has been marked to be cancelled within the database." }, { status: 200 });
+	try {
+		const response = await fetch(supaUrl, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+				apiKey: apiKey as string,
+				Authorization: anonKey as string,
+			},
+			body: JSON.stringify({
+				user_id: user_id,
+				cancel_requested_on: requestedDate,
+				cancel_end_date: endDate,
+			}),
+		});
+        
+        console.log(response);
 
+		if (!response.ok) {
+			throw new Error(`HTTP error!  Status: ${response.status}`);
+		}
+
+		return NextResponse.json({ message: "The user has been marked to be cancelled within the database." }, { status: 200 });
     } catch (e: unknown) {
-        return NextResponse.json({ message: `The following error occurred in updating the cancellation table: ${e instanceof Error && e.message}` }, { status: 400 });
-    }
+		return NextResponse.json({ message: `The following error occurred in updating the cancellation table: ${e instanceof Error && e.message}` }, { status: 400 });
+	}
 }
