c0d098a3430f5d3b6946dbb22cc27886725b7cff
Chris <banksjazz87@gmail.com>
2026-09-14 08:45:48 -0400

    feature: working on the billing layout

--- app/account/billing/components/BillingTable.tsx
+++ app/account/billing/components/BillingTable.tsx
@@ -14,44 +14,42 @@ type BillingTableProps = {
 export default function BillingTable({ invoices }: BillingTableProps): JSX.Element {
 
 	const invoiceData = use(invoices);
-	
-	if (invoiceData.success) {
-		console.log(invoiceData.data);
-	}
+	console.log("Invoice Data HERE: ", invoiceData);
+
 
 	return (
 		<section className="mt-4">
-			{<p>No Data found</p>}
-			<Table>
-				<TableCaption>A list of your most recent transactions.</TableCaption>
-				<TableHeader>
-					<TableRow>
-						<TableHead className="font-bold">Invoice #</TableHead>
-						<TableHead className="font-bold">Description</TableHead>
-						<TableHead className="font-bold">Billing Period</TableHead>
-						<TableHead className="font-bold">Amount</TableHead>
-						<TableHead className="font-bold">Status</TableHead>
-						<TableHead className="font-bold">Invoice Date</TableHead>
-						<TableHead className="font-bold">Actions</TableHead>
-						<TableHead className="center"></TableHead>
-					</TableRow>
-				</TableHeader>
-				<TableBody>
-					{/* {invoiceData.status === 200 &&
-						invoiceData((data: Stripe.Subscription, y: number) => (
-							<TableRow key={`thread_num_${y}`}>
-								<TableCell>{StripeProducts.get(data.items.data[0].plan.product as string)}</TableCell>
-								<TableCell className="capitalize">{`${data.items.data[0].plan.interval}ly`}</TableCell>
-								<TableCell>{data.items.data[0].plan.amount ? `$${data.items.data[0].plan.amount / 100}` : "$0.00"}</TableCell>
-								<TableCell>{getDate(data.start_date)}</TableCell>
-								<TableCell>{getNextBillingDate(data.billing_cycle_anchor)}</TableCell>
-								<TableCell>{data.canceled_at ? getDate(data.canceled_at) : "-"}</TableCell>
-								<TableCell>{data.cancel_at ? getDate(data.cancel_at) : "-"}</TableCell>
-								<TableCell className="capitalize"></TableCell>
-								<TableCell> 
-							</TableRow> */}
-				</TableBody>
-			</Table>
+			{!invoiceData.success && <p>No Data Found</p>}
+			{invoiceData.success && (
+				<Table>
+					<TableCaption>A list of your most recent transactions.</TableCaption>
+					<TableHeader>
+						<TableRow>
+							<TableHead className="font-bold">Invoice #</TableHead>
+							<TableHead className="font-bold">Description</TableHead>
+							<TableHead className="font-bold">Billing Period</TableHead>
+							<TableHead className="font-bold">Amount</TableHead>
+							<TableHead className="font-bold">Status</TableHead>
+							<TableHead className="font-bold">Invoice Date</TableHead>
+							<TableHead className="font-bold">Actions</TableHead>
+							<TableHead className="center"></TableHead>
+						</TableRow>
+					</TableHeader>
+					<TableBody>
+						{invoiceData.data.map((data: Stripe.Invoice, y: number) => (
+							<TableRow key={`row_num_${y}`}>
+								<TableCell></TableCell>
+								<TableCell></TableCell>
+								<TableCell></TableCell>
+								<TableCell></TableCell>
+								<TableCell></TableCell>
+								<TableCell></TableCell>
+								<TableCell></TableCell>
+							</TableRow>
+						))}
+					</TableBody>
+				</Table>
+			)}
 		</section>
 	);
 }
